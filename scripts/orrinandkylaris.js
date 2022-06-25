b195c0179912cbab1b84e863ceb1232512200558
dan76-20 <mar18109@byui.edu>
2022-06-25 17:13:07 -0600

    working on phone friendly design

--- scripts/orrinandkylaris.js
+++ scripts/orrinandkylaris.js
@@ -1,22 +1,22 @@
       new Twitch.Embed("orrinn", {
-        width: 854,
-        height: 480,
+        width: 360,
+        height: 203,
         channel: "orrinn",
         // Only needed if this page is going to be embedded on other websites
         parent: ["embed.example.com", "othersite.example.com"]
       });
 
       new Twitch.Embed("selfishlock", {
-        width: 854,
-        height: 480,
+        width: 360,
+        height: 203,
         channel: "selfishlock",
         // Only needed if this page is going to be embedded on other websites
         parent: ["embed.example.com", "othersite.example.com"]
       });
 
       new Twitch.Embed("beercityboardgames", {
-        width: 854,
-        height: 480,
+        width: 360,
+        height: 203,
         channel: "beercityboardgames",
         // Only needed if this page is going to be embedded on other websites
         parent: ["embed.example.com", "othersite.example.com"]
