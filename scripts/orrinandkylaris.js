ed60dcb9518e2f62756468a8755e63089e577431
dan76-20 <mar18109@byui.edu>
2022-06-25 17:01:14 -0600

    first commit

---
+++ scripts/orrinandkylaris.js
@@ -0,0 +1,23 @@
+      new Twitch.Embed("orrinn", {
+        width: 854,
+        height: 480,
+        channel: "orrinn",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
+
+      new Twitch.Embed("selfishlock", {
+        width: 854,
+        height: 480,
+        channel: "selfishlock",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
+
+      new Twitch.Embed("beercityboardgames", {
+        width: 854,
+        height: 480,
+        channel: "beercityboardgames",
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"]
+      });
