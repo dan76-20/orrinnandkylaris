d1d8e729c534e6d3c4bc71bbd9fbbdd750d8b9aa
dan76-20 <mar18109@byui.edu>
2022-06-25 19:38:04 -0600

    images added along with more css and html for footer

--- scripts/orrinandkylaris.js
+++ scripts/orrinandkylaris.js
@@ -1,23 +1,35 @@
       new Twitch.Embed("orrinn", {
-        width: 360,
-        height: 203,
+        width: 854,
+        height: 480,
         channel: "orrinn",
         // Only needed if this page is going to be embedded on other websites
         parent: ["embed.example.com", "othersite.example.com"]
       });
 
       new Twitch.Embed("selfishlock", {
-        width: 360,
-        height: 203,
+        width: 854,
+        height: 480,
         channel: "selfishlock",
         // Only needed if this page is going to be embedded on other websites
         parent: ["embed.example.com", "othersite.example.com"]
       });
 
       new Twitch.Embed("beercityboardgames", {
-        width: 360,
-        height: 203,
+        width: 854,
+        height: 480,
         channel: "beercityboardgames",
         // Only needed if this page is going to be embedded on other websites
         parent: ["embed.example.com", "othersite.example.com"]
       });
+
+      var today = new Date();
+      var day = today.getDate();
+      var month = today.getMonth()+1;
+      var year = today.getFullYear();
+      if (day < 10) {
+        day = '0'+day;
+      }
+      if (month < 10) {
+        month = '0' + month;
+      }
+      document.querySelector('#date').innerHTML='Orrinn and Kylaris &copy;' + month + "/" + day + "/" + year;
